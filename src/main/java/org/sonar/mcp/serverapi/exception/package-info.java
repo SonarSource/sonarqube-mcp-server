ffa70d64f5a966ab2d0a445b63b7af97fd6cc587
Nicolas QUINQUENEL <nicolas.quinquenel@sonarsource.com>
2025-05-16 10:57:44 +0200

    Fix license

--- src/main/java/org/sonar/mcp/serverapi/exception/package-info.java
+++ src/main/java/org/sonar/mcp/serverapi/exception/package-info.java
@@ -1,7 +1,7 @@
 /*
- * SonarLint Core - Server API
- * Copyright (C) 2016-2025 SonarSource SA
- * mailto:info AT sonarsource DOT com
+ * Sonar MCP Server
+ * Copyright (C) 2025 SonarSource
+ * sonarlint@sonarsource.com
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -13,9 +13,9 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
  */
 @ParametersAreNonnullByDefault
 package org.sonar.mcp.serverapi.exception;
