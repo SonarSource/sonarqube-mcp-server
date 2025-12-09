1cd444921b99e868c1f8ece5b336f9629a47004c
Nicolas QUINQUENEL <nicolas.quinquenel@sonarsource.com>
2025-12-09 10:54:10 +0100

    Update and fix tests for PHP analyzer and backend

--- src/test/java/org/sonarsource/sonarqube/mcp/harness/SonarQubeMcpServerTestHarness.java
+++ src/test/java/org/sonarsource/sonarqube/mcp/harness/SonarQubeMcpServerTestHarness.java
@@ -177,7 +177,7 @@ public class SonarQubeMcpServerTestHarness extends TypeBasedParameterResolver<So
           "plugins": [
             {
               "key": "php",
-              "filename": "sonar-php-plugin-3.52.0.15197.jar",
+              "filename": "sonar-php-plugin-3.53.0.15220.jar",
               "sonarLintSupported": true
             }
           ]
@@ -185,7 +185,7 @@ public class SonarQubeMcpServerTestHarness extends TypeBasedParameterResolver<So
       """)));
     try {
       mockSonarQubeServer.stubFor(get(PluginsApi.DOWNLOAD_PLUGINS_PATH + "?plugin=php")
-        .willReturn(aResponse().withBody(Files.readAllBytes(Paths.get("build/sonarqube-mcp-server/plugins/sonar-php-plugin-3.52.0.15197.jar")))));
+        .willReturn(aResponse().withBody(Files.readAllBytes(Paths.get("build/sonarqube-mcp-server/plugins/sonar-php-plugin-3.53.0.15220.jar")))));
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
