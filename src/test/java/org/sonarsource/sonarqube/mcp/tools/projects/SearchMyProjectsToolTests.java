732c802b805cdc366338e51530e0ae356039c325
Nicolas Quinquenel <nicolas.quinquenel@sonarsource.com>
2026-02-06 09:50:24 +0100

    Fix tests

--- src/test/java/org/sonarsource/sonarqube/mcp/tools/projects/SearchMyProjectsToolTests.java
+++ src/test/java/org/sonarsource/sonarqube/mcp/tools/projects/SearchMyProjectsToolTests.java
@@ -75,7 +75,7 @@ class SearchMyProjectsToolTests {
                "description":"Pagination information for the results"
             },
             "projects":{
-               "description":"List of projects found in the organization",
+               "description":"List of projects found",
                "type":"array",
                "items":{
                   "type":"object",
